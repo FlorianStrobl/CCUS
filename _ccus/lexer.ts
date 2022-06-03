3e7841ce14a63d0468adf64cbee67e30c0fb7231
Florian Strobl <FlorianStrobl@outlook.de>
2022-06-03 12:30:17 +0200

    1

--- _ccus/lexer.ts
+++ _ccus/lexer.ts
@@ -18,9 +18,9 @@ export namespace lexer {
     whitespaces = 'whitespace', // any of: ["\n", " ", "\t"]
     comment = 'comment', // "// singleline comment" or "/*multiline comment*/" (are considered whitespace)
     keyword = 'keyword', // reserved identifiers: ["func", "bit", ...]
-    symbol = 'symbol', // reserved non-alphanumeric identifiers: ["+", "-", "()", ...]
-    literal = 'literal', // any of these types: [true, 0, 0.0, 'a', "hello world"]
-    identifier = 'id' // alphanumeric words like "myFunction", "myVariable"
+    symbol = 'symbol', // reserved non-alphanumeric identifiers with length 1 or 2: ["+", "-", "()", ...]
+    literal = 'literal', // any of these types: [0, 0.0, 'a', "hello world"]
+    identifier = 'id' // alphanumeric words like "myFunction", "myVariable", "true", "false"
   }
 
   const symbols: str[] = [
@@ -35,23 +35,26 @@ export namespace lexer {
     ']',
     '=',
     '==',
-    '+'
+    '+',
+    '<='
   ];
 
   const keywords: str[] = ['bit', 'func', 'for', 'return'];
 
+  const stringMod: char[] = ['r', 'c'];
+
   let currentIndex: int = 0;
-  let currentLineCount: int = 1; // TODO, lower scope?
   let code: str = '';
   // #endregion
 
   export function lexer(source: str): lexem[] {
-    const time: int = Date.now();
+    const startTime: int = Date.now();
 
     code = source;
     const lexems: lexem[] = [];
     let invalidLexems: [char: char, index: int][] = [];
 
+    // #region main loop
     for (let i = 0; i < source.length; ++i) {
       if (currentIndex !== i)
         console.error(
@@ -59,14 +62,13 @@ export namespace lexer {
         );
 
       if (isWhitespace(curChar())) {
-        // do nothing
-        if (curChar() === '\n') currentLineCount++; // increase line count
+        // whitespace check => do nothing
       } else if (
         curChar() === '/' &&
         (nextChars() === '/' || nextChars() === '*')
       ) {
+        // comment check => captchure the comment
         const startingIndex: int = currentIndex;
-        const startingLineCount: int = currentLineCount;
 
         const ans = eatComment(nextChars() === '/');
 
@@ -76,18 +78,33 @@ export namespace lexer {
           content: ans.comment,
           type: tokenType.comment,
           index: startingIndex,
-          line: startingLineCount,
-          column: getColumne(startingIndex)
+          line: getLinePos(startingIndex),
+          column: getColumnPos(startingIndex)
         });
-      } else if (isSymbol(curChar())) {
-        // could be a symbol with multipe characters:
+      } else if (!!curChar().match(/^"$/)) {
+        // started string
+        const startingIndex: int = currentIndex;
+
+        const ans = eatString();
+
+        i += ans.charCount;
+
+        lexems.push({
+          content: ans.str,
+          type: tokenType.literal,
+          index: startingIndex,
+          line: getLinePos(startingIndex),
+          column: getColumnPos(startingIndex)
+        });
+      } else if (startLikeSymbol(curChar())) {
+        // symbol check => check if two character symbol
         if (isSymbol(curChar() + nextChars())) {
           lexems.push({
             content: curChar() + nextChars(),
             type: tokenType.symbol,
             index: currentIndex,
-            line: currentLineCount,
-            column: getColumne()
+            line: getLinePos(),
+            column: getColumnPos()
           });
 
           // because next character already consumed
@@ -98,15 +115,29 @@ export namespace lexer {
             content: curChar(),
             type: tokenType.symbol,
             index: currentIndex,
-            line: currentLineCount,
-            column: getColumne()
+            line: getLinePos(),
+            column: getColumnPos()
           });
         }
+      } else if (isDigit(curChar())) {
+        // is a numeric literal
+        const startingIndex: int = currentIndex;
+
+        const ans = eatNumber();
+
+        i += ans.charCount;
+
+        lexems.push({
+          content: ans.number,
+          type: tokenType.literal,
+          index: startingIndex,
+          line: getLinePos(startingIndex),
+          column: getColumnPos(startingIndex)
+        });
       } else if (isAlpha(curChar())) {
         // is identifier or keyword
 
         const startingIndex: int = currentIndex;
-        const startingLineCount: int = currentLineCount;
 
         const ans = eatIdentifier();
 
@@ -118,56 +149,81 @@ export namespace lexer {
             ? tokenType.keyword
             : tokenType.identifier,
           index: startingIndex,
-          line: startingLineCount,
-          column: getColumne(startingIndex)
+          line: getLinePos(startingIndex),
+          column: getColumnPos(startingIndex)
         });
       } else {
         //console.error(
-        //  `[lexer] unresolved character at line ${currentLineCount} columne ${getColumne()}: "${curChar()}"`
+        //  `[lexer] unresolved character at line ${currentLineCount} column ${getColumn()}: "${curChar()}"`
         //);
         invalidLexems.push([curChar(), currentIndex]);
       }
 
       advance(); // increase the currentIndex
     }
-
-    console.log(`[lexer] finished lexing in ${Date.now() - time} ms`);
-
-    // #region invalid lexems
-    // search for characters which are directly one after another
-    let toRemoveIndexes: int[] = [];
-    for (let i = 0; i < invalidLexems.length; ++i) {
-      let index = invalidLexems[i][1];
-      if (invalidLexems.length > i + 1 && index + 1 === invalidLexems[i + 1][1])
-        toRemoveIndexes.push(index + 1);
-    }
-
-    // TODO, multiple toRemoveIndexes one after another lmao
-    console.log(toRemoveIndexes);
-    for (let i = 0; i < toRemoveIndexes.length; ++i)
-      for (let y = 0; y < invalidLexems.length; ++y)
-        if (invalidLexems[y][1] === toRemoveIndexes[i])
-          invalidLexems[y - 1][0] += invalidLexems[y][0];
-
-    invalidLexems = invalidLexems.filter((lexem) =>
-      toRemoveIndexes.includes(lexem[1])
-    );
-
-    console.log(invalidLexems);
-
-    if (invalidLexems.length !== 0)
-      console.error(
-        `[lexer] lexer found invalid tokens: ${invalidLexems.map(
-          ([char, index]) =>
-            `\ncharacter at position ${index} is invalid: ${char}`
-        )}`
-      );
     // #endregion
+
+    console.log(`[lexer] finished lexing in ${Date.now() - startTime} ms`);
+
+    printErrors(invalidLexems);
 
     return lexems;
   }
 
+  function printErrors(invalidLexems: [char: char, index: int][]): void {
+    if (invalidLexems.length === 0) return;
+
+    /**
+     * get errors from each line, generate a ^^ message underling all of the mistakes
+     * (check for column==0 and column==end...)
+     * make it dynamic such that the error msg can be adjusted fastly
+     */
+
+    // #region merge following characters, TODO needed if
+    // search for characters which are directly one after another
+    let toRemoveIndexes: int[] = [];
+    invalidLexems.forEach((lexem, index) => {
+      if (
+        invalidLexems.length > index + 1 &&
+        lexem[1] + 1 === invalidLexems[index + 1][1]
+      )
+        toRemoveIndexes.push(index + 1);
+    });
+
+    // multiple toRemoveIndexes one after another lmao
+    for (let i = invalidLexems.length - 1; i >= 0; --i)
+      if (toRemoveIndexes.includes(i))
+        invalidLexems[toRemoveIndexes[toRemoveIndexes.indexOf(i)] - 1][0] +=
+          invalidLexems[toRemoveIndexes[toRemoveIndexes.indexOf(i)]][0];
+
+    invalidLexems = invalidLexems.filter(
+      (_, index) => !toRemoveIndexes.includes(index)
+    );
+    // #endregion
+
+    if (invalidLexems.length !== 0) {
+      console.error(`[lexer] lexer found invalid tokens:`);
+
+      
+      for (let i = 0; i < invalidLexems.length; ++i) {
+        const lexem: [substr: string, index: number] = invalidLexems[i];
+        const lineContent: str = getLine(lexem[1]);
+        const column: int = getColumnPos(lexem[1]);
+        const line: int = getLinePos(lexem[1]);
+        let showing: str = ' '.repeat(column - 1) + '^'.repeat(lexem[0].length);
+
+        const msg1: str = `invalid token${
+          lexem[0].length === 1 ? '' : 's'
+        } at line ${line}: "`;
+        console.log(
+          `${msg1}${lineContent}"\n${' '.repeat(msg1.length) + showing}`
+        );
+      }
+    }
+  }
+
   // #region helper functions
+  // #region meta functions
   function curChar(): char {
     return code[currentIndex];
   }
@@ -176,62 +232,133 @@ export namespace lexer {
     return code.slice(currentIndex + 1, currentIndex + 1 + count);
   }
 
-  function advance(): void {
+  function advance(): bool {
     currentIndex++;
+    return currentIndex === code.length;
   }
+  // #endregion
 
+  // #region eat
+  // TODO check if line end
   function eatComment(singleLineComment: bool): {
     charCount: int;
     comment: str;
   } {
-    let charCount: int = 0;
     let comment: str = '';
 
     while (true) {
       if (singleLineComment) {
-        if (curChar() === '\n') {
-          currentLineCount++;
-          break;
-        } else {
-          comment += curChar();
-          advance();
-          charCount++;
-        }
+        if (curChar() === '\n') break;
+        comment += curChar();
+        if (advance()) break;
       } else {
         if (curChar() === '*' && nextChars() === '/') {
           comment += '*/'; // do this, as it isnt done anymore...
-          advance(); // advance, because of "*"
-          charCount++;
+          if (advance()) break; // advance, because of "*"
           break;
         } else {
-          if (curChar() === '\n') currentLineCount++;
           comment += curChar();
-          advance();
-          charCount++;
+          if (advance()) break;
         }
       }
     }
 
-    return { charCount, comment };
+    return { charCount: comment.length - (singleLineComment ? 0 : 1), comment };
   }
 
   function eatIdentifier(): {
     charCount: int;
     identifier: str;
   } {
-    let charCount: int = 0;
     let identifier: str = '';
 
-    while (true) {
-      if (!isAlphaNumeric(curChar())) break;
-      charCount++;
+    while (isAlphaNumeric(curChar())) {
       identifier += curChar();
-      advance();
+      if (advance())
+        // is at line end!
+        break;
     }
 
-    return { charCount, identifier };
+    currentIndex--; // because we eat one too much!!
+    return { charCount: identifier.length - 1, identifier };
   }
 
+  function eatNumber(): { charCount: int; number: str } {
+    let number: str = curChar();
+
+    let gotDot: bool = false;
+    let gotE: bool = false;
+    let lastCharWasE: bool = false; // for optionall sign after the e
+
+    // look into the future
+    while (true) {
+      if (lastCharWasE) {
+        lastCharWasE = false;
+        if (!!nextChars().match(/[+-]/)) {
+          number += nextChars();
+          advance();
+          continue;
+        } // else just continue
+      }
+
+      if (isDigit(nextChars())) {
+        number += nextChars();
+        advance();
+      } else if (!!nextChars().match(/\./) && !gotDot && !gotE) {
+        gotDot = true;
+        number += nextChars();
+        advance();
+      } else if (!!nextChars().match(/e/i) && !gotE) {
+        gotE = true;
+        lastCharWasE = true;
+        number += nextChars();
+        advance();
+      } else break;
+    }
+
+    return { charCount: number.length - 1, number };
+  }
+
+  function eatString(): { charCount: int; str: str } {
+    let str: str = '';
+
+    while (true) {
+      // handle "\n", "\t", \uxxxx and escapes (\)
+      if (curChar() !== '\\' && nextChars() === '"') {
+        // stop string next char
+        str += curChar();
+        advance();
+
+        // check if the next char is a single and valid string type character
+        if (
+          stringMod.includes(nextChars()) &&
+          // check if it is not an identifier/keyword
+          (nextChars(2)[1] === undefined || !isAlphaNumeric(nextChars(2)[1]))
+        ) {
+          str += '"' + nextChars();
+          advance();
+        } else str += '"';
+
+        break;
+      } else {
+        str += curChar();
+        advance();
+      }
+    }
+
+    return {
+      charCount: str.length - 1, // check first the size
+      str: str // then modify it
+        .replace(/\\n/g, '\n')
+        .replace(/\\t/g, '\t')
+        .replace(/\\u(\d{4})/, (_, digits) =>
+          String.fromCharCode(Number('0x' + (digits as string)))
+        )
+    };
+  }
+  // #endregion
+
+  // #region is
   function isWhitespace(char: char): bool {
     return !!char.match(/^(?: |\t|\n)$/);
   }
@@ -248,10 +375,6 @@ export namespace lexer {
     return !!char.match(/^[0-9]$/);
   }
 
-  function isNumeric(char: char): bool {
-    return !!char.match(/^[0-9eE+-.]$/);
-  }
-
   function startLikeSymbol(char: char): bool {
     // only checks for the first character of every symbol
     return symbols.map((str) => str[0]).includes(char);
@@ -260,21 +383,47 @@ export namespace lexer {
   function isSymbol(str: str): bool {
     return symbols.includes(str);
   }
+  // #endregion
 
-  function getColumne(index: int = currentIndex): int {
+  // #region get position
+  function getLinePos(index: int = currentIndex): int {
+    return (code.slice(0, index + 1).match(/\n/g) || []).length + 1;
+  }
+
+  function getColumnPos(index: int = currentIndex): int {
     // offset - charCountUntilLastLine
     return index - code.slice(0, index + 1).lastIndexOf('\n');
     // (lastIndexOf===-1) is ok,
     // as it is (- (-1)) => (index + 1); and this is occuring then (index===0)
   }
+
+  // get the line in which this index exists
+  function getLine(index: int = currentIndex): str {
+    if (index >= code.length) return '';
+
+    let ans: str = '';
+    let inStr: bool = false;
+
+    for (let i = 0; i < code.length; ++i) {
+      if (i === index) inStr = true;
+
+      if (code[i] === '\n')
+        if (!inStr) ans = '';
+        else break;
+      else ans += code[i];
+    }
+
+    return ans;
+  }
+  // #endregion
   // #endregion
 }
 
 console.log(
-  lexer.lexer(`+ // cm
-func(x) return lama
-/*b*///
-===~!~/**/
-c #// line 9
-$$`)
+  lexer.lexer(`
+
+a "regexp"r f
+  $* 2 ~
+
+  "hello \\" world" ~`)
 );
